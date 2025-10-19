7bad8aca91d8e2b62078a53f6b235cb42ae24f23
Millan Kumar <48894200+LelsersLasers@users.noreply.github.com>
2025-10-19 12:57:46 -0400

    CAN view displays the last received of each message  by id instead of every message in a list
    * Can viewer has a hashmap of messages, saving only the last one
    
    * One row per signal instead of per message and a dict of signals
    
    * Update Cargo.lock?

--- src/app.rs
+++ src/app.rs
@@ -114,5 +114,8 @@ impl eframe::App for DAQApp {
         ui::sidebar::show(self, ctx);
 
         workspace::show(self, ctx);
+
+        // Probally could to a return chain to not always request repaint
+        ctx.request_repaint();
     }
 }
--- src/ui/can_viewer.rs
+++ src/ui/can_viewer.rs
@@ -1,19 +1,17 @@
 use crate::{can, ui};
 use eframe::egui;
-use std::collections::VecDeque;
-
-const CAN_VIEWER_MAX_DECODED_MSGS: usize = 1000; // TODO: have a scroll back -> query db
+use hashbrown::HashMap;
 
 pub struct CanViewer {
     pub title: String,
-    pub decoded_msgs: VecDeque<can::message::ParsedMessage>,
+    pub decoded_msgs: HashMap<u32, can::message::ParsedMessage>,
 }
 
 impl CanViewer {
     pub fn new(instance_num: usize) -> Self {
         Self {
             title: format!("CAN Viewer #{}", instance_num),
-            decoded_msgs: VecDeque::with_capacity(CAN_VIEWER_MAX_DECODED_MSGS),
+            decoded_msgs: HashMap::new(),
         }
     }
 
@@ -24,43 +22,59 @@ impl CanViewer {
         egui_extras::TableBuilder::new(ui)
             .striped(true)
             .column(egui_extras::Column::auto().at_least(100.0).resizable(true)) // Timestamp
-            .column(egui_extras::Column::auto().at_least(100.0).resizable(true)) // ID
-            .column(egui_extras::Column::remainder().resizable(true)) // Decoded Content
+            .column(egui_extras::Column::auto().at_least(300.0).resizable(true)) // Msg (ID)
+            .column(egui_extras::Column::auto().at_least(200.0).resizable(true)) // Signal
+            .column(egui_extras::Column::remainder().resizable(true)) // Decoded Signal
             .header(20.0, |mut header| {
                 header.col(|ui| {
                     ui.label("Timestamp");
                 });
                 header.col(|ui| {
-                    ui.label("ID");
+                    ui.label("Msg (ID)");
+                });
+                header.col(|ui| {
+                    ui.label("Signal");
                 });
                 header.col(|ui| {
                     ui.label("Decoded Content");
                 });
             })
-            .body(|body| {
-                let row_count = self.decoded_msgs.len();
-                // rows is more performant than iter + row
-                body.rows(18.0, row_count, |mut row| {
-                    let idx = row_count - row.index() - 1; // Reverse order
-                    let msg = &self.decoded_msgs[idx];
-                    row.col(|ui| {
-                        ui.label(msg.timestamp.format("%H:%M:%S:%3f").to_string());
-                    });
-                    row.col(|ui| {
-                        ui.label(format!("0x{:X}", msg.decoded.msg_id));
-                    });
-                    row.col(|ui| {
-                        let signals_str = msg
-                            .decoded
-                            .signals
-                            .iter()
-                            .map(|(name, sig)| format!("{}: {} {}", name, sig.value, sig.unit))
-                            .collect::<Vec<String>>()
-                            .join(", ");
-                        ui.label(signals_str.to_string());
-                    });
-                });
+            .body(|mut body| {
+                let msg_keys = self.decoded_msgs.keys().cloned().collect::<Vec<_>>();
+                let mut sorted_msg_keys = msg_keys;
+                sorted_msg_keys.sort();
+                for msg_id in sorted_msg_keys {
+                    let msg = &self.decoded_msgs[&msg_id];
+                    let mut signal_keys = msg.decoded.signals.keys().cloned().collect::<Vec<_>>();
+                    signal_keys.sort();
+                    for signal_name in signal_keys {
+                        let signal = &msg.decoded.signals[&signal_name];
+                        body.row(18.0, |mut row| {
+                            row.col(|ui| {
+                                ui.label(msg.timestamp.format("%H:%M:%S:%3f").to_string());
+                            });
+                            row.col(|ui| {
+                                ui.label(format!(
+                                    "{} (0x{:X})",
+                                    msg.decoded.name, msg.decoded.msg_id
+                                ));
+                            });
+                            row.col(|ui| {
+                                ui.label(signal.name.to_string());
+                            });
+                            row.col(|ui| {
+                                if signal.unit.is_empty() {
+                                    ui.label(format!("{}", signal.value));
+                                } else {
+                                    ui.label(format!("{} {}", signal.value, signal.unit));
+                                }
+                            });
+                        });
+                    }
+                }
             });
+
+        ui.scroll_to_cursor(Some(egui::Align::Min));
 
         egui_tiles::UiResponse::None
     }
@@ -68,10 +82,8 @@ impl CanViewer {
     pub fn handle_can_message(&mut self, msg: &can::can_messages::CanMessage) {
         match msg {
             can::can_messages::CanMessage::ParsedMessage(parsed_msg) => {
-                if self.decoded_msgs.len() == CAN_VIEWER_MAX_DECODED_MSGS {
-                    self.decoded_msgs.pop_front();
-                }
-                self.decoded_msgs.push_back(parsed_msg.clone());
+                self.decoded_msgs
+                    .insert(parsed_msg.decoded.msg_id, parsed_msg.clone());
             }
             _ => {}
         }
