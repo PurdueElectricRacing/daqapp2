4216be86e53b45b4d77ece759b0987ce2135d903
Millan Kumar <48894200+LelsersLasers@users.noreply.github.com>
2025-10-19 12:41:05 -0400

    Can viewer has a hashmap of messages, saving only the last one

--- src/ui/can_viewer.rs
+++ src/ui/can_viewer.rs
@@ -1,19 +1,18 @@
 use crate::{can, ui};
 use eframe::egui;
-use std::collections::VecDeque;
-
-const CAN_VIEWER_MAX_DECODED_MSGS: usize = 1000; // TODO: have a scroll back -> query db
+// use std::collections::VecDeque;
+use hashbrown::HashMap;
 
 pub struct CanViewer {
     pub title: String,
-    pub decoded_msgs: VecDeque<can::message::ParsedMessage>,
+    pub decoded_msgs: HashMap<u32, can::message::ParsedMessage>,
 }
 
 impl CanViewer {
     pub fn new(instance_num: usize) -> Self {
         Self {
             title: format!("CAN Viewer #{}", instance_num),
-            decoded_msgs: VecDeque::with_capacity(CAN_VIEWER_MAX_DECODED_MSGS),
+            decoded_msgs: HashMap::new(),
         }
     }
 
@@ -24,43 +23,43 @@ impl CanViewer {
         egui_extras::TableBuilder::new(ui)
             .striped(true)
             .column(egui_extras::Column::auto().at_least(100.0).resizable(true)) // Timestamp
-            .column(egui_extras::Column::auto().at_least(100.0).resizable(true)) // ID
+            .column(egui_extras::Column::auto().at_least(300.0).resizable(true)) // Msg (ID)
             .column(egui_extras::Column::remainder().resizable(true)) // Decoded Content
             .header(20.0, |mut header| {
                 header.col(|ui| {
                     ui.label("Timestamp");
                 });
                 header.col(|ui| {
-                    ui.label("ID");
+                    ui.label("Msg (ID)");
                 });
                 header.col(|ui| {
                     ui.label("Decoded Content");
                 });
             })
-            .body(|body| {
-                let row_count = self.decoded_msgs.len();
-                // rows is more performant than iter + row
-                body.rows(18.0, row_count, |mut row| {
-                    let idx = row_count - row.index() - 1; // Reverse order
-                    let msg = &self.decoded_msgs[idx];
-                    row.col(|ui| {
-                        ui.label(msg.timestamp.format("%H:%M:%S:%3f").to_string());
+            .body(|mut body| {
+                for msg in self.decoded_msgs.values() {
+                    body.row(18.0, |mut row| {
+                        row.col(|ui| {
+                            ui.label(msg.timestamp.format("%H:%M:%S:%3f").to_string());
+                        });
+                        row.col(|ui| {
+                            ui.label(format!("{} (0x{:X})", msg.decoded.name, msg.decoded.msg_id));
+                        });
+                        row.col(|ui| {
+                            let signals_str = msg
+                                .decoded
+                                .signals
+                                .iter()
+                                .map(|(name, sig)| format!("{}: {} {}", name, sig.value, sig.unit))
+                                .collect::<Vec<String>>()
+                                .join(", ");
+                            ui.label(signals_str.to_string());
+                        });
                     });
-                    row.col(|ui| {
-                        ui.label(format!("0x{:X}", msg.decoded.msg_id));
-                    });
-                    row.col(|ui| {
-                        let signals_str = msg
-                            .decoded
-                            .signals
-                            .iter()
-                            .map(|(name, sig)| format!("{}: {} {}", name, sig.value, sig.unit))
-                            .collect::<Vec<String>>()
-                            .join(", ");
-                        ui.label(signals_str.to_string());
-                    });
-                });
+                }
             });
+
+        ui.scroll_to_cursor(Some(egui::Align::Min));
 
         egui_tiles::UiResponse::None
     }
@@ -68,10 +67,8 @@ impl CanViewer {
     pub fn handle_can_message(&mut self, msg: &can::can_messages::CanMessage) {
         match msg {
             can::can_messages::CanMessage::ParsedMessage(parsed_msg) => {
-                if self.decoded_msgs.len() == CAN_VIEWER_MAX_DECODED_MSGS {
-                    self.decoded_msgs.pop_front();
-                }
-                self.decoded_msgs.push_back(parsed_msg.clone());
+                self.decoded_msgs
+                    .insert(parsed_msg.decoded.msg_id, parsed_msg.clone());
             }
             _ => {}
         }
