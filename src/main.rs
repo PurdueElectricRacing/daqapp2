1611b5e20271495662e1e23cb79ba8b5e9c4cb8a
Pris1320 <prisha.k.mohan@gmail.com>
2026-01-22 19:56:52 -0500

    remove comments

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,10 @@ fn main() -> eframe::Result<()> {
 
     let _can_thread = can::thread::start_can_thread(can_sender, ui_receiver);
 
+
     let icon =
         from_png_bytes(include_bytes!("../images/PER_Logo.png")).expect("Failed to load app icon");
+
 
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_icon(icon),
