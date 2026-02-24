d2f51c770bfbc29e4abc2a666abecff8d42fa8a8
Irving Wang <irvingw@purdue.edu>
2026-02-24 13:41:20 -0500

    cleanup

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -2,6 +2,5 @@ pub mod bootloader;
 pub mod log_parser;
 pub mod scope;
 pub mod sidebar;
-pub mod ui_messages;
 pub mod viewer_list;
 pub mod viewer_table;
--- src/ui/ui_messages.rs
+++
@@ -1,7 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-pub enum ConnectionSource {
-    Serial(String),
-    Udp(u16),
-}
