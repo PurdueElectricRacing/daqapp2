ec671f352ccb89b5c9718152afe5db514c6337c3
LelsersLasers <millankumar@gmail.com>
2026-04-04 15:25:40 -0400

    Improve comment

--- src/util.rs
+++ src/util.rs
@@ -19,6 +19,10 @@ pub fn get_available_serial_ports() -> Vec<serialport::SerialPortInfo> {
 }
 
 pub mod msg_id {
+    // Converts a can_dbc::MessageId to a u32, setting the extended ID flag if it's an extended ID
+    // The extended ID flag is the highest bit (32nd bit) of the u32.
+    // Standard IDs (11 bits) will have this bit unset, while extended IDs (29 bits) will have this bit set.
+    // Generally use this version when interfacing with `can_decode`
     pub fn can_dbc_to_u32_with_extid_flag(msg_id: &can_dbc::MessageId) -> u32 {
         match msg_id {
             can_dbc::MessageId::Standard(id) => *id as u32,
@@ -26,9 +30,12 @@ pub mod msg_id {
         }
     }
 
+    // Converts a can_dbc::MessageId to a u32 without setting the extended ID flag
+    // Ex: a 29-bit extended ID will use at most 29 bits while the other version of this function
+    // would set the highest bit to indicate it's an extended ID.
     pub fn can_dbc_to_u32_without_extid_flag(msg_id: &can_dbc::MessageId) -> u32 {
         match msg_id {
-            can_dbc::MessageId::Standard(id) => *id as u32,
+            can_dbc::MessageId::Standard(id) => *id as u32 & 0x7FF,
             can_dbc::MessageId::Extended(id) => *id & 0x7FFFFFFF,
         }
     }
